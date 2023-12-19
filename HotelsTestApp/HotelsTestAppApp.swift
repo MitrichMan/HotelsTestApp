5a96d820217be2881ecc6bd5567613a57df7dc5d
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-12-19 09:51:31 -0300

    Initial Commit

---
+++ HotelsTestApp/HotelsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelsTestAppApp.swift
+//  HotelsTestApp
+//
+//  Created by Dmitrii Melnikov on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
