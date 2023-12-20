84cea5b084a2a52dad632f03d7cd06e519932bb7
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-12-20 10:28:28 -0300

    Added DetailedData view

---
+++ HotelsTestApp/HotelView/DetailedDataView/DetailedDataView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedDataView.swift
+//  HotelsTestApp
+//
+//  Created by Dmitrii Melnikov on 19.12.2023.
+//
+
+import SwiftUI
+
+struct DetailedDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailedDataView()
+}
