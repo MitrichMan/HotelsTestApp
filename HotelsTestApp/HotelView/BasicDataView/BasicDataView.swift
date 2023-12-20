5573232b0df085da745c06cfd2bb57aa56fba091
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-12-20 11:31:17 -0300

    Work with carousel

--- HotelsTestApp/HotelView/BasicDataView/BasicDataView.swift
+++ HotelsTestApp/HotelView/BasicDataView/BasicDataView.swift
@@ -30,24 +30,28 @@ struct BasicDataView: View {
     private let mokPhotos = ["Image1", "Image2", "Image3", "Image4", "Image5"]
     
     var body: some View {
+        
         VStack {
+            
             Text("Отель")
                 .font(.system(size: 18, weight: .medium))
-            ScrollView(.horizontal) {
-                HStack {
+            
+            TabView {
                     ForEach((0...mokPhotos.count - 1), id: \.self) { index in
                         Image(mokPhotos[index])
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .frame(width: 343, height: 257)
                     }
-                }
-            }.padding(.horizontal, 16)
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            .frame(height: 257)
+            .cornerRadius(15)
+            
         }
-        
-        
     }
 }
+
 
 
 #Preview {
