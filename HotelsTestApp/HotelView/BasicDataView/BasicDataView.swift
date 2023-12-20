d7d6cad89991ff185bf62cb8524dbff6f2ab509b
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-12-20 16:01:08 -0300

    Work with BasicData view.

--- HotelsTestApp/HotelView/BasicDataView/BasicDataView.swift
+++ HotelsTestApp/HotelView/BasicDataView/BasicDataView.swift
@@ -27,33 +27,70 @@ struct BasicDataView: View {
         serviceCharge: 15
     )
     
-    private let mokPhotos = ["Image1", "Image2", "Image3", "Image4", "Image5"]
-    
     var body: some View {
         
-        VStack {
+        VStack(spacing: 8) {
             
             Text("Отель")
                 .font(.system(size: 18, weight: .medium))
             
-            TabView {
-                    ForEach((0...mokPhotos.count - 1), id: \.self) { index in
-                        Image(mokPhotos[index])
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                    }
-            }
-            .tabViewStyle(PageTabViewStyle())
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            .frame(height: 257)
-            .cornerRadius(15)
+            CarouselView()
+                .padding(.bottom, 8)
             
-        }
+                MarkView()
+            
+            Text(mokHotel.hotelName)
+            
+            }
+        .padding(.horizontal, 16)
     }
 }
 
-
-
 #Preview {
     BasicDataView()
+}
+
+struct CarouselView: View {
+    
+    private let mokPhotos = ["Image1", "Image2", "Image3", "Image4", "Image5"]
+
+    var body: some View {
+        TabView {
+
+            ForEach((0...mokPhotos.count - 1), id: \.self) { index in
+                Image(mokPhotos[index])
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        .frame(height: 257)
+        .cornerRadius(15)
+    }
+}
+
+struct MarkView: View {
+    var body: some View {
+        HStack {
+            ZStack {
+                Color("MarkBackground")
+                
+                HStack(spacing: 2) {
+                    Image("MarkStar")
+                        .resizable()
+                        .frame(width: 16.0, height: 16.0)
+                        .aspectRatio(contentMode: .fit)
+                        
+                    Text("5 Превосходно")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundStyle(.mark)
+                }
+            }
+            .frame(width: 149, height: 29)
+            .cornerRadius(5)
+            
+            Spacer()
+        }
+    }
 }
